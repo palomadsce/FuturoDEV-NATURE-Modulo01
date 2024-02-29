853695157c17b19cec408602670e64b2ceca233e
palomadsce <palomads86@gmail.com>
2024-02-29 16:51:44 -0300

    Create ex03.js

---
+++ Semana04/Resolucao exercicios trello/ex03.js
@@ -0,0 +1,14 @@
+//SEMANA 04
+// EXERCICIO 3
+const exercicio = (array) => {
+    let soma = 0;
+    for (let numero of array){
+        soma += numero;
+    }
+    let media = soma/array.length;
+    console.log(media);
+}
+
+let arrayNumeros = [10,10,10,10]
+
+exercicio(arrayNumeros)
