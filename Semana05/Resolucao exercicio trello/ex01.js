bb767096edd4e1a586291ba4009fa1d818aac434
palomadsce <palomads86@gmail.com>
2024-03-01 14:41:19 -0300

    Update ex01.js
    Resolucao ex 01

--- Semana05/Resolucao exercicio trello/ex01.js
+++ Semana05/Resolucao exercicio trello/ex01.js
@@ -1,14 +1,18 @@
 //Semana 05
-//Exercicio 01 trello
+
+//Exercicio 01- Acesso simples
+//Crie um array chamado frutas contendo três frutas diferentes.
+// Em seguida, imprima no console a segunda fruta do array.
+//Desafio: utilize a biblioteca prompt-sync para receber cada uma das 3 frutas.
+
 //Resolução simples
 //const frutas = [ "Banana", "Abacate", "Mamão"]
 //console.log(frutas[1])
+
 //Resolução usando prompt-sync
 const prompt = require("prompt-sync")()
-
 //Criando uma array chamando a fruta
 const frutas = []
-
 //Solicitando ao usuario para inserir as frutas
 for (let i = 0; i < 3; i ++){
     frutas.push(prompt("Digite o nome da fruta: "))
