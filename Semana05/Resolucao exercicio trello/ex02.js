fd02b00bdd24acb25a498e835fdb329456b98dbd
palomadsce <palomads86@gmail.com>
2024-03-01 14:41:27 -0300

    Create ex02.js
    Resolucao ex 02

---
+++ Semana05/Resolucao exercicio trello/ex02.js
@@ -0,0 +1,21 @@
+//Semana 05
+
+//Exercicio 02- Adicionar e Remover Elementos
+//Usando o mesmo array do exercício anterior, adicione mais uma fruta ao array frutas.
+// Em seguida, remova a primeira fruta do array. 
+//Imprima o array atualizado no console. 
+
+//Resolução usando prompt-sync
+
+const prompt = require("prompt-sync")({sigint: true})
+const frutas = []
+
+for(let i = 0; i < 3; i++){
+  frutas.push(prompt("Digite o nome da fruta: "))
+}
+
+frutas.push(prompt("Digite uma fruta adicional: "))
+
+frutas.shift()
+
+console.log(frutas)
