232837501c13de53ba9c227a742d6ca0e561db9d
palomadsce <palomads86@gmail.com>
2024-03-01 14:37:19 -0300

    Update ex03.js
    Resolucao ex 03

--- Semana04/Resolucao exercicios trello/ex03.js
+++ Semana04/Resolucao exercicios trello/ex03.js
@@ -1,5 +1,8 @@
-//SEMANA 04
-// EXERCICIO 3
+// SEMANA 04
+
+// EXERCICIO 3 Crie uma arrow function que recebe um array de números e calcula a média deles.
+// OBS: para pegar o tamanho de um array use nomeDoArray.length
+
 const exercicio = (array) => {
     let soma = 0;
     for (let numero of array){
