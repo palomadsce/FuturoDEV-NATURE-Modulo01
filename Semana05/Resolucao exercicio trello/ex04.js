37c23647e9c9897122332072478a1ee857b39d51
palomadsce <palomads86@gmail.com>
2024-03-01 14:41:51 -0300

    Create ex04.js
    Resolucao ex 04

---
+++ Semana05/Resolucao exercicio trello/ex04.js
@@ -0,0 +1,20 @@
+//Semana 05
+
+//Exercicio 04 - Soma de Elementos
+//Utilizando o array numeros do exercício anterior, 
+//calcule e imprima a soma de todos os elementos do array. 
+//Utilize o reduce para realizar a soma.
+
+const prompt = require("prompt-sync")({sigint: true})
+const numeros = []
+
+for(let i = 0; i < 5; i++){
+  const numero = parseInt(prompt(`Digite o ${i + 1} número: `));
+  numeros.push(numero);
+}
+const soma = numeros.reduce((acumulador, numeroAtual) => {
+    return acumulador + numeroAtual
+  }, 0)
+
+  
+  console.log("A soma dos números é ",soma)
