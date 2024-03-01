67d2d7aceb7212fbe8f7067d6700251fbe47aaa0
palomadsce <palomads86@gmail.com>
2024-03-01 14:38:06 -0300

    Create ex06.js
    Resolucao ex 06

---
+++ Semana04/Resolucao exercicios trello/ex06.js
@@ -0,0 +1,13 @@
+// SEMANA 04
+
+// EXERCICIO 6 
+//Utilizando estrutura de repetição for realize a tabuada do número 5 e exiba no console da seguinte forma:
+//5 x 0 = 0
+//5 x 1 = 5
+//5 x 2 = 10
+//....
+//5 x 10 = 50
+
+for(let i=0; i<11; i++){
+    console.log("5 x " + i + " = " + 5*i);
+  }
