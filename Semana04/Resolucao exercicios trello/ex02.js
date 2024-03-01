9214e58de85a4d9e83c660738b3ca2c6434892d7
palomadsce <palomads86@gmail.com>
2024-03-01 14:36:59 -0300

    Create ex02.js
    Resolucao ex 02

---
+++ Semana04/Resolucao exercicios trello/ex02.js
@@ -0,0 +1,16 @@
+// SEMANA 04
+
+// EXERCICIO 2 Crie uma função nomeada que recebe um número e devolva uma string dizendo se é par ou ímpar.
+
+//Chamando o prompt
+const prompt = require ('prompt-sync')()
+
+
+//Funcao receber numero
+var n = prompt("Digite um numero: ");
+var resto = n % 2;
+if (resto == 0) {
+    console.log("O número é par");
+} else {
+    console.log("O número é impar")
+}
