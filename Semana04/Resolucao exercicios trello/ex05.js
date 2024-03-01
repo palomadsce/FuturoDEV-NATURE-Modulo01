0621fd1471f7908333d2cb3cbe575aac39eb4e00
palomadsce <palomads86@gmail.com>
2024-03-01 14:37:48 -0300

    Create ex05.js
    Resolucao ex 05

---
+++ Semana04/Resolucao exercicios trello/ex05.js
@@ -0,0 +1,20 @@
+// SEMANA 04
+
+// EXERCICIO 5 
+//Utilizando um while, repita uma mini entrevista com 4 usuários perguntando qual a nota para avaliação da série "Stranger Things”, 
+//será apenas aceita três tipos de avaliação: ruim, bom e excelente.
+/// Depois verifique quantas pessoas classificaram a série como ruim.
+
+const prompt = require ('prompt-sync')()
+let totalRuins = 0;
+let contador = 1;
+while (contador <= 4){
+    let resposta = prompt("Qual a asua avaliação para a a serie stranger things? (bom), (ruim) ou (excelente)");
+    if(resposta == "ruim" ) {
+        totalRuins++;
+    }
+
+    contador++;
+}
+
+console.log(`Total de usuários que avaliou a série como ruim: ${totalRuins}`);
